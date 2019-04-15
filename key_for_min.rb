e715e7b8a1301e666797cdd30813592410e8d2b7
Yasmine Hartung <y.hartung@gmail.com>
2019-04-15 23:06:59 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,20 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+ min_val
+ 
+  name_hash.collect do |name, value|
+    if min_val == nil
+      min_val = value
+      min_name = name
 
+    else
+      if min_val > value
+        min_val = value
+        min_name = name
+        
+      
+    end
+  end
+  min_name
 end
